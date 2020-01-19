2b3aef1c107d895d9f03d5933b30132f2f912d49
Afonso Amaro <doominating@gmail.com>
2020-01-19 03:30:36 -0300

    add cors on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const moongoose = require('mongoose');
+const cors = require('cors');
 const routes = require ('./routes');
 
 const app = express();
@@ -12,6 +13,7 @@ moongoose.connect(
         useCreateIndex: true,
     }
 );
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
