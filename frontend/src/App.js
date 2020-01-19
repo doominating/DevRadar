11826c1a64ec0d5d4d4ebfc991fe3481e6d8edb1
Afonso Amaro <doominating@gmail.com>
2020-01-19 02:42:24 -0300

    added main app structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/8459716?s=460&v=4"
+                alt="Afonso Amaro"
+                className="src"
+              />
+              <div className="user-info">
+                <strong>Afonso Amaro</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Software Engineer</p>
+            <a href="https://github.com/doominating">
+              Acessar perfil no Github
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/8459716?s=460&v=4"
+                alt="Afonso Amaro"
+                className="src"
+              />
+              <div className="user-info">
+                <strong>Afonso Amaro</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Software Engineer</p>
+            <a href="https://github.com/doominating">
+              Acessar perfil no Github
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/8459716?s=460&v=4"
+                alt="Afonso Amaro"
+                className="src"
+              />
+              <div className="user-info">
+                <strong>Afonso Amaro</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Software Engineer</p>
+            <a href="https://github.com/doominating">
+              Acessar perfil no Github
+            </a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars2.githubusercontent.com/u/8459716?s=460&v=4"
+                alt="Afonso Amaro"
+                className="src"
+              />
+              <div className="user-info">
+                <strong>Afonso Amaro</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Software Engineer</p>
+            <a href="https://github.com/doominating">
+              Acessar perfil no Github
+            </a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
