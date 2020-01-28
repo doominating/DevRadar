108a754b351789798e71d52218de597580c7e5ae
Afonso Amaro <doominating@gmail.com>
2020-01-28 02:16:01 -0300

    Add initial location and marker of the map

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,87 @@
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from "react-native-maps";
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
-  return <MapView style={{ flex:1 }}/>
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -20.8047562, longitude: -49.3587704 }}>
+        <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/8459716?s=460&v=4' }}></Image>
+
+        <Callout onPress={() => {
+          
+        }}>
+          <View style={styles.callout}>
+            <Text style={styles.devName}> Afonso Amaro</Text>
+            <Text style={styles.devBio}>Software Engineer</Text>
+            <Text style={styles.devTechs}>ReactJS, React Native, Node.JS</Text>
+          </View>
+
+        </Callout>
+      </Marker>
+    </MapView>
+  );
 }
+const styles = StyleSheet.create({
+  map: {
+    flex:1
+  },
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF'
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  devTechs: {
+    marginTop: 5,
+  }
+
+});
 
 export default Main;
