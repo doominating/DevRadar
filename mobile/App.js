de104974cbd073ed66e191b841d9b325e7b4b761
Afonso Amaro <doominating@gmail.com>
2020-01-28 01:30:19 -0300

    Create basic routes components and MapView

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Status, StatusBar } from 'react-native';
+
+import Routes from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello OmnisStack!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7"/>
+      <Routes />
+    </>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
